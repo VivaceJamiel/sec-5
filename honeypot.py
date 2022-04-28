cd00e6f728680ee0bf13452aaf0870a50e83068e
VivaceJamiel <jimpoy2@gmail.com>
2022-04-27 22:58:12 -0500

    ssh finished

--- honeypot.py
+++ honeypot.py
@@ -12,11 +12,10 @@ from binascii import hexlify
 from pathlib import Path
 
 attempts = 0
+running = True
 
-class SubSystemHandler(paramiko.SubsystemHandler):
-    def start_subsystem(self, name, transport, channel):
-        self.event.set()
-        return super().start_subsystem(name, transport, channel)        
+def ssh_command_handler(command):
+    print('default: ', command)
 
 class Server(paramiko.ServerInterface):
     def __init__(self):
@@ -28,75 +27,86 @@ class Server(paramiko.ServerInterface):
     def check_auth_password(self, username, password):
         global attempts
         attempts += 1
-        if (attempts < 5):
-            return paramiko.AUTH_FAILED
-        else:
-            return paramiko.AUTH_SUCCESSFUL
+        # if (attempts < 5):
+        #     print(attempts)
+        #     return paramiko.AUTH_FAILED
+        return paramiko.AUTH_SUCCESSFUL
 
     def check_auth_publickey(self, username, key):
         return paramiko.AUTH_SUCCESSFUL
 
+    def check_channel_pty_request(self, channel, term, width, height, pixelwidth, pixelheight, modes):
+        return True
 
-# class Server(paramiko.ServerInterface):
-#     def __init__(self):
-#         self.event = threading.Event()
+    def check_channel_shell_request(self, channel):
+        self.event.set()
+        return True
 
-#     def check_channel_request(self, kind, chan_id):
-#         if kind == "session":
-#             return paramiko.OPEN_SUCCEEDED
-#         return paramiko.OPEN_FAILED_ADMINISTRATIVELY_PROHIBITED
+def init():
+    if "-p" in sys.argv:
+        try:
+            port = sys.argv[sys.argv.index("-p") + 1]
 
-#     def check_auth_password(self, username, password):
-#         global attempts
-#         attempts += 1
-#         if (attempts < 5):
-#             print(attempts)
-#             return paramiko.AUTH_FAILED
-#         print(attempts)
-#         return paramiko.AUTH_SUCCESSFUL
+            sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 
-#     def check_auth_publickey(self, username, key):
-#         print("Auth attempt with key: " + u(hexlify(key.get_fingerprint())))
-#         if (username == "robey") and (key == self.good_pub_key):
-#             return paramiko.AUTH_SUCCESSFUL
-#         return paramiko.AUTH_FAILED
+            sock.bind(("127.0.0.1", int(port)))
 
-#     def check_auth_gssapi_with_mic(self, username, gss_authenticated=paramiko.AUTH_FAILED, cc_file=None):
-#         if gss_authenticated == paramiko.AUTH_SUCCESSFUL:
-#             return paramiko.AUTH_SUCCESSFUL
-#         return paramiko.AUTH_FAILED
+            while True:
+                sock.listen(100)
+                client, addr = sock.accept()
 
-#     def check_channel_shell_request(self, channel):
-#         self.event.set()
-#         return True
+                t = paramiko.Transport(client)
+                if os.path.exists('./honeypot.key'):
+                    t.add_server_key(paramiko.RSAKey.from_private_key_file('./honeypot.key'))        
+                else:
+                    key = paramiko.RSAKey.generate(2048)
+                    key.write_private_key_file('./honeypot.key')
+                    t.add_server_key(key)
+                server = Server()
+                t.start_server(server=server)
+                
+                # accept the connection and set its idle timeout
+                chan = t.accept(20)
+                chan.settimeout(60)
+                
+                # Get the user of the connection
+                username = t.get_username()
 
+                if chan is None:
+                    print('Timeout waiting for channel')
+                    continue
+                print(username + " connected")
+
+                chan.send("Welcome to the Honeypot!\n")     
+                run = True
+                while run:
+                    chan.send("\r")
+                    chan.send(username + "@" + addr[0] +"$ ")
+                    command = ""
+                    while not command.endswith("\r"):
+                        transport = chan.recv(1024)
+                        chan.send(transport)
+                        command += transport.decode("utf-8")
+                    chan.send("\r\n")
+                    command = command.rstrip()
+                    print(command)
+                    if command == "exit":
+                        run = False
+                    else:
+                        print("other command")
+
+                chan.close()
+                t.close()
+                print("Closed connection")
+        except KeyboardInterrupt:
+            print("\nInterrupt received, exiting...")
+            sys.exit(1)
+        except Exception as e:
+            print("user is inactive")
+            sys.exit(1)
+    else:   
+        print("Please specify a port with -p")
 
 if __name__ == "__main__":
-    if "-p" in sys.argv:
-        port = sys.argv[sys.argv.index("-p") + 1]
-        sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-        sock.bind(("127.0.0.1", int(port)))
-        sock.listen(100)
-        print("Listening for connection...")
-        client, addr = sock.accept()
-        print("Got a connection!")
-        t = paramiko.Transport(client)        
-        if os.path.exists('./honeypot.key'):
-            t.add_server_key(paramiko.RSAKey.from_private_key_file('./honeypot.key'))        
-        else:
-            key = paramiko.RSAKey.generate(2048)
-            key.write_private_key_file('./honeypot.key')
-            t.add_server_key(key)
-
-        server = Server()
-
-        try:
-            t.start_server(server=server)
-        except paramiko.SSHException:
-            print("*** SSH negotiation failed.")
-            sys.exit(1)
-        
-    else:
-        print("No port specified")
-        sys.exit(1)
+    init()
